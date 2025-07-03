df3a7d5e6bcf8fd92413a009547aee5dfd7ca12e
Zu15mi <mizuumi2001@gmail.com>
2025-07-04 01:54:24 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,17 @@
+//calculator 
+import java.io.*;
+class calculator 
+{
+ public static void main(String args[]) throws IOException
+{
+int a=20;
+int b=6;
+System.out.println("numbers are :" + a+ "and" +b);
+System.out.println("sum :" +(a+b));
+System.out.println("sub :" +(a-b));
+System.out.println("multiple :" +(a*b));
+System.out.println("division :" +(a / b));
+System.out.println("(a//b) :" +(a / b));
+System.out.println("(a%b) :" +(a % b));
+}
+}
---
+++ example1.java
@@ -0,0 +1,9 @@
+//example1.java.Write a program in java to display "Welcome to St. Xaviers College"
+import java.io.*;
+class example1 
+{
+ public static void main (String args[])  throws IOException
+{ 
+ System.out.println("Welcome to St. Xaviers College");
+}
+}
